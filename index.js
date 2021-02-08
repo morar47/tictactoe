dd1f773b0c0198ac9ee9938a28a4a938b850c744
morar47 <30659790+morar47@users.noreply.github.com>
2021-02-07 19:56:59 -0500

    finishing js for now....

--- index.js
+++ index.js
@@ -35,3 +35,59 @@ function startGame() {
   setBoardHoverClass()
   winningMessageElement.classList.remove('show')
 }
+
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+      endGame(false)
+    } else if (isDraw()) {
+      endGame(true)
+    } else {
+      swapTurns()
+      setBoardHoverClass()
+    }
+  }
+  
+  function endGame(draw) {
+    if (draw) {
+      winningMessageTextElement.innerText = 'Draw!'
+    } else {
+      winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+    }
+    winningMessageElement.classList.add('show')
+  }
+  
+  function isDraw() {
+    return [...cellElements].every(cell => {
+      return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+    })
+  }
+  
+  function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+  }
+  
+  function swapTurns() {
+    circleTurn = !circleTurn
+  }
+  
+  function setBoardHoverClass() {
+    board.classList.remove(X_CLASS)
+    board.classList.remove(CIRCLE_CLASS)
+    if (circleTurn) {
+      board.classList.add(CIRCLE_CLASS)
+    } else {
+      board.classList.add(X_CLASS)
+    }
+  }
+  
+  function checkWin(currentClass) {
+    return WINNING_COMBINATIONS.some(combination => {
+      return combination.every(index => {
+        return cellElements[index].classList.contains(currentClass)
+      })
+    })
+  }
